17b51c862d240e76f7fa0f9714b950a5a4da7a70
Killmina <maka00745@yahoo.co.jp>
2018-10-07 22:31:49 +0900

    inputの内容を追加しました

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 
 struct Data {
 	int year;		//年
@@ -23,6 +23,34 @@ void Menu(){
 
 //森、大谷担当//
 void Input() {
+	char ymd[10];
+	fa=fopen("Calorie.txt","a");
+	if(fa==NULL){
+		printf("ファイルが開けませんでした！\n");
+	}
+	printf("＊食事の新規追加を行います＊\n");
+	printf("＊（「0」で入力を終了します）＊\n");
+	printf("＊食事した日付を入力してください＊\n");
+	printf("＊（例：2018年1月1日→2018/01/01）＊\n");
+	scanf("%s",&ymd[10]);
+	printf("＊では食事の入力を開始します！＊\n");
+	printf("＊何も食べていない時は「0」を入力してください！＊\n");
+	printf("＊%d年%d月%d日の朝食はなんですか？＊\n",year,month,day);
+	scanf("%s",&morning);
+	printf("朝食のカロリーはいくらですか？＊\n");
+	scanf("%d",&morningcalorie);
+	printf("＊%d年%d月%d日の昼食はなんですか？＊\n",year,month,day);
+	scanf("%s",&lunch);
+	printf("昼食のカロリーはいくらですか？＊\n");
+	scanf("%d",&lunchcalorie);
+	printf("＊%d年%d月%d日の夕食はなんですか？＊\n",year,month,day);
+	scanf("%s",&dinner);
+	printf("夕食のカロリーはいくらですか？＊\n");
+	scanf("%d",&dinnercalorie);
+	printf("＊%d年%d月%d日の間食はなんですか？＊\n",year,month,day);
+	scanf("%s",&break);
+	printf("夕食のカロリーはいくらですか？＊\n");
+	scanf("%d",&break);
 
 }
 
